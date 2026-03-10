b266547f542262638313428ad626c8cab4d62053
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-03-09 22:12:01 -0700

    -

--- main.sh
+++ main.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -uo pipefail
-main=src/main.rkt
-racket "$main"
+run() {
+	out=logs/out/$2.log
+	err=logs/err/$2.log
+	racket "$1" \
+		1> "$out" \
+		2> "$err"
+}
+rm -rf logs
+mkdir -p logs/{out,err}
+run src/main.rkt run
+find . -empty -delete
--- src/main.rkt
+++ src/main.rkt
@@ -1,20 +1,67 @@
 #lang racket/gui
-(define main
-	(new frame%
-		[label "My Racket GUI App"]
-	)
+(define (random-el list)
+	(list-ref list (random (length list)))
 )
-(define button
-	(new button%
-		[parent main]
-		[label "Click Me!"]
-		[callback (lambda
-			(button event)
-			(message-box
-				"Hello"
-				"You clicked the button!"
-			)
-		)]
-	)
+(define (create-dragon texture colour)
+	(define dragon (make-hash))
+	(hash-set! dragon 'texture texture)
+	(hash-set! dragon 'colour colour)
+	dragon
 )
+(define dragons (list))
+(define textures (list
+	'Scales
+	'Feathers
+))
+(define colours (list
+	'Red
+	'Orange
+	'Yellow
+	'Green
+	'Blue
+	'Purple
+	'Pink
+	'Brown
+	'White
+	'Grey
+	'Black
+	'Gold
+	'Silver
+	'Bronze
+))
+(define title "Dragons!")
+(define main (new frame%
+	[label title]
+))
+(define button (new button%
+	[parent main]
+	[label "Create new dragon"]
+	[callback (lambda (button event)
+		(define dragon (create-dragon
+			(random-el textures)
+			(random-el colours)
+		))
+		(set! dragons (reverse (cons dragon (reverse dragons))))
+		(new button%
+			[parent main]
+			[label (string-append
+				"Show Dragon#"
+				(number->string (length dragons))
+			)]
+			[callback (lambda (button event)
+				; (new frame%)
+				(message-box
+					(string-append
+						"Dragon#"
+						(number->string (length dragons))
+					)
+					(string-join (list
+						(symbol->string (hash-ref dragon 'texture))
+						(symbol->string (hash-ref dragon 'colour))
+					) "\n")
+				)
+			)]
+		)
+	)]
+))
 (send main show #t)
